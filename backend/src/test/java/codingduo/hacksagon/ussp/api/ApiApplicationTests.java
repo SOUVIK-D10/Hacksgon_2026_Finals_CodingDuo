e01656186f88c869583ca2b1f0e0d35126f4703e
Souvik-D10 <svkc.cht@gmail.com>
2026-04-03 18:13:36 +0530

    Backend Template done

---
+++ backend/src/test/java/codingduo/hacksagon/ussp/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package codingduo.hacksagon.ussp.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
